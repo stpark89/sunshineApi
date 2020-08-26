9d53548cfa5b7857434a66d27b83a77360f169f9
moyeong-gyu <moyeong-gyu@moyeong-gyuui-macbook-pro.local>
2020-08-26 23:03:57 +0900

    rec

--- src/main/java/com/sun/api/server/rec/repository/RecRepository.java
+++ src/main/java/com/sun/api/server/rec/repository/RecRepository.java
@@ -8,10 +8,10 @@ import com.sun.api.server.vo.RecVo;
 
 public interface RecRepository extends JpaRepository<RecVo, Long>{
 	
-	@Query(value = "SELECT * FROM rec_vo order by trade_date DESC limit 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM rec_vo where division = 0 order by trade_date DESC limit 1", nativeQuery = true)
 	RecVo findBySearchLastRecData();
 	
-	@Query(value = "SELECT * FROM rec_vo order by trade_date DESC limit 1", nativeQuery = true)
+	@Query(value = "SELECT * FROM rec_vo where division = 1 order by trade_date DESC limit 1", nativeQuery = true)
 	RecVo findBySearchLastJejuRecdData();
 	
 	@Query(value = "SELECT count(1) FROM rec_vo WHERE trade_date_string =:tradeDateString", nativeQuery = true)
