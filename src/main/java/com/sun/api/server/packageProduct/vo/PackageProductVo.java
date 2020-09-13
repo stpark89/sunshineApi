c2d7fced71984f030c89dd86f2d016e867f4a42e
stpark89 <stpark89@naver.com>
2020-09-13 17:18:50 +0900

    vo 변경

--- src/main/java/com/sun/api/server/packageProduct/vo/PackageProductVo.java
+++ src/main/java/com/sun/api/server/packageProduct/vo/PackageProductVo.java
@@ -25,17 +25,12 @@ public class PackageProductVo {
     // 기준용량
     private double kw;
 
-    // 모듈 아이디
-    private long moudleId;
     //모듈 제조사
     private String moduleManufacturer;
     //모듈 품명
     private String moduleProductName;
     // 모듈 로고 - 이미지 url
     private String moduleImageUrl;
-
-    // 인버터 아이디
-    private long inverterId;
 
     // 인버터 제조사
     private String inverterManufacturer;
